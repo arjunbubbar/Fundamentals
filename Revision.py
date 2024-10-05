9a03a7930c3bf7dac8802fbda9648412c51c7514
arjunbubbar <arjunbubbar@gmail.com>
2024-10-05 13:50:41 +0100

    Add files via upload

---
+++ Revision.py
@@ -0,0 +1,35 @@
+print ("Hello")
+
+number = int (input ("What is your number? "))
+
+def evenorodd (number):
+    if number % 2 == 0:
+        print ("Number is even")
+    else:
+        print ("Number is odd")
+
+evenorodd (number)
+evenorodd (123)
+
+
+natural = int (input ("What is your number? "))
+
+for i in range (1, natural + 1):
+    print (i)
+
+start = 0
+while start <= natural:
+    print (start)
+    start = start + 1 
+
+list = [1, 25, 36, 79, 102]
+
+print (list [0])
+
+for i in range (0, len (list)):
+    print (list [i])
+
+
+
+    
+
